720b2c4b6afdddbf2835030c8ecf1bc4ee7c4f5f
Guilherme Bustamante <guilherme_bustamante@live.com>
2023-08-16 12:14:27 -0300

    feat: configure routes using react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,43 @@
-function App() {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold underline">DriveX</h1>
-    </div>
-  );
-}
+import { createBrowserRouter } from "react-router-dom";
+import { Home } from "./pages/home";
+import { Login } from "./pages/login";
+import { Register } from "./pages/register";
+import { Dashboard } from "./pages/dashboard";
+import { New } from "./pages/dashboard/new";
+import { CarDetail } from "./pages/car";
 
-export default App;
+import { Layout } from "./components/layout";
+
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/car/:id",
+        element: <CarDetail />,
+      },
+      {
+        path: "/dashboard",
+        element: <Dashboard />,
+      },
+      {
+        path: "/dashboard/new",
+        element: <New />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+]);
+
+export { router };
