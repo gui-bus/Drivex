0164f3ce590779172b196eec4f31beb970daab40
Guilherme Bustamante <guilherme_bustamante@live.com>
2023-08-17 18:52:56 -0300

    feat: add route protection dashboard dashboard/new

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { New } from "./pages/dashboard/new";
 import { CarDetail } from "./pages/car";
 
 import { Layout } from "./components/layout";
+import { Private } from "./routes/Private";
 
 const router = createBrowserRouter([
   {
@@ -22,11 +23,21 @@ const router = createBrowserRouter([
       },
       {
         path: "/dashboard",
-        element: <Dashboard />,
+        element: (
+          <Private>
+            {" "}
+            <Dashboard />{" "}
+          </Private>
+        ),
       },
       {
         path: "/dashboard/new",
-        element: <New />,
+        element: (
+          <Private>
+            {" "}
+            <New />{" "}
+          </Private>
+        ),
       },
     ],
   },
---
+++ src/routes/Private.tsx
@@ -0,0 +1,26 @@
+import { ReactNode, useContext } from "react";
+import { AuthContext } from "../contexts/authContext";
+import { Navigate } from "react-router-dom";
+import { BarLoader } from "react-spinners";
+
+interface PrivateProps {
+  children: ReactNode;
+}
+
+export function Private({ children }: PrivateProps): any {
+  const { signed, loadingAuth } = useContext(AuthContext);
+
+  if (loadingAuth) {
+    return (
+      <div className="loading-container">
+        <BarLoader color={"#2dd4bf"} loading={loadingAuth} />
+      </div>
+    );
+  }
+
+  if (!signed) {
+    return <Navigate to="/login" />;
+  }
+
+  return children;
+}
