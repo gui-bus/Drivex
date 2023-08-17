75ad2958aa24dbd880f1f90f3fc38333bd2a37e3
Guilherme Bustamante <guilherme_bustamante@live.com>
2023-08-17 20:24:23 -0300

    style: add red color variant

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,9 +10,11 @@ export default {
         lightBlue: "#249C98",
         darkGray: "#222423",
         lightGray: "#DCE0DD",
+        mainRed: "#DC3237",
+        mainRedLighter: "#FF454A",
       },
       scale: {
-        '102': '1.02',
+        102: "1.02",
       },
     },
   },
