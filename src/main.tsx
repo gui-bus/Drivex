ce33a9901bea6bf092d20a45fab0ced739c66698
Guilherme Bustamante <guilherme_bustamante@live.com>
2023-08-22 19:16:52 -0300

    feat: add Toaster from react hot toast

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,12 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import AuthProvider from "./contexts/authContext.tsx";
 
+import { Toaster } from "react-hot-toast";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <Toaster position="bottom-center" reverseOrder={false} />
+
     <AuthProvider>
       <RouterProvider router={router} />
     </AuthProvider>
