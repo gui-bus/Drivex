9e89b25d4544dd6edfb379b67f6314e607d1a72a
Guilherme Bustamante <guilherme_bustamante@live.com>
2023-08-22 20:26:39 -0300

    feat: add cars to home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,51 +1,161 @@
+import { useState, useEffect } from "react";
+
 import { Container } from "../../components/container";
 import { BsSearch } from "react-icons/bs";
+import { GrLocation } from "react-icons/gr";
+
+import { collection, query, getDocs, orderBy } from "firebase/firestore";
+import { db } from "../../services/firebaseConnection";
+
+import { Link } from "react-router-dom";
+import { BarLoader } from "react-spinners";
+
+interface CarsProps {
+  id: string;
+  name: string;
+  model: string;
+  version: string;
+  year: string;
+  uid: string;
+  price: string | number;
+  city: string;
+  state: string;
+  km: string;
+  images: CarImageProps[];
+}
+
+interface CarImageProps {
+  name: string;
+  uid: string;
+  url: string;
+}
 
 export function Home() {
+  const [cars, setCars] = useState<CarsProps[]>([]);
+  const [loadImages, setLoadImages] = useState<string[]>([]);
+
+  useEffect(() => {
+    function loadCars() {
+      const carsRef = collection(db, "vehicles");
+      const queryRef = query(carsRef, orderBy("created", "desc"));
+
+      getDocs(queryRef).then((snapshot) => {
+        const listcars = [] as CarsProps[];
+
+        snapshot.forEach((doc) => {
+          listcars.push({
+            id: doc.id,
+            name: doc.data().name,
+            model: doc.data().model,
+            version: doc.data().version,
+            year: doc.data().year,
+            km: doc.data().km,
+            city: doc.data().city,
+            state: doc.data().state,
+            price: doc.data().price,
+            images: doc.data().images,
+            uid: doc.data().uid,
+          });
+        });
+
+        setCars(listcars);
+      });
+    }
+
+    loadCars();
+  }, []);
+
+  function handleImageLoad(id: string) {
+    setLoadImages((prevImageLoaded) => [...prevImageLoaded, id]);
+  }
+
   return (
     <Container>
-      <section className="bg-white p-4 rounded-lg w-full max-w-3xl mx-auto flex justify-center items-center gap-2">
-        <input
-          type="text"
-          placeholder="Digite o nome do veículo..."
-          className="w-full border-2 rounded-lg h-9 px-3 outline-none"
-        />
-        <button className="px-2 transition-all duration-200 ease-linear hover:scale-110">
-          <BsSearch size={24} color="#222423" />
-        </button>
+      <section className="bg-gray-100 py-16 px-6 rounded-lg w-full max-w-3xl mx-auto flex flex-col justify-center items-center gap-6 text-center -mt-10">
+        <p className="text-xl text-gray-600 max-w-xl font-semibold mb-2">
+          Explore a melhor seleção de carros novos e usados em todo o Brasil.
+          Encontre o veículo dos seus sonhos!
+        </p>
+
+        <div className="flex w-full gap-2 bg-white p-3 rounded-md">
+          <input
+            type="text"
+            placeholder="Digite o nome do veículo..."
+            className="w-full border-2 rounded-lg h-9 px-3 outline-none"
+          />
+          <button className="px-2 transition-all duration-200 ease-linear hover:scale-110">
+            <BsSearch size={24} color="#222423" />
+          </button>
+        </div>
       </section>
 
-      <h1 className="font-semibold text-center my-6 text-xl">
-        Navegue pela maior seleção de carros novos e usados em todo o Brasil!
-      </h1>
+      <main className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 text-center">
+        {cars.map((car) => (
+          <Link key={car.id} to={`/car/${car.id}`}>
+            <section className="w-full bg-white rounded-lg hover:scale-102 transition-all duration-200 ease-linear cursor-pointer shadow-lg">
+              <div
+                className="w-full h-56 max-h-72 rounded-lg relative"
+                style={{
+                  display: loadImages.includes(car.id) ? "none" : "block",
+                }}
+              >
+                <div className="absolute inset-0 flex items-center justify-center">
+                  <BarLoader color={"#DC3237"} />
+                </div>
+              </div>
 
-      <main className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 text-center">
-        <section className="w-full bg-white rounded-lg hover:scale-102 transition-all duration-200 ease-linear cursor-pointer shadow-lg">
-          <img
-            src="https://image.webmotors.com.br/_fotos/anunciousados/gigante/2023/202308/20230805/ferrari-sf90-3.9-v8-turbo-phev-stradale-f1dct-wmimagem14292271233.jpg?s=fill&w=552&h=414&q=60"
-            alt="Veiculo"
-            className="w-full rounded-t-lg mb-2 max-h-72 object-cover"
-          />
-          <p className="font-bold mt-1 px-2">FERRARI SF90</p>
-          <p className="text-zinc-700 px-2 text-sm">
-            3.9 V8 TURBO PHEV STRADALE F1-DCT
-          </p>
+              <img
+                src={car.images[0].url}
+                alt="Veiculo"
+                className="w-full rounded-t-lg mb-2 h-56 max-h-72 object-cover"
+                onLoad={() => handleImageLoad(car.id)}
+                style={{
+                  display: loadImages.includes(car.id) ? "block" : "none",
+                }}
+              />
+              <p className="font-bold my-2 px-2 uppercase lg:text-sm">
+                {car.name} {car.model}
+              </p>
+              <p className="text-zinc-500 px-2 text-xs uppercase">
+                {car.version}
+              </p>
 
-          <div className="flex flex-col px-2 text-sm">
-            <span className="text-zinc-700 mb-3">
-              Ano 2021/2022 | 23.000 km
-            </span>
-            <strong className="text-black font-medium text-2xl">
-              R$ 5.550.000
-            </strong>
-          </div>
+              <p className="text-zinc-500 px-2 text-xs">
+                {car.year} | {parseFloat(car.km).toLocaleString("pt-BR")} km
+              </p>
 
-          <div className="w-full h-px bg-slate-200 my-2"></div>
+              <div className="flex flex-col px-2 text-sm my-3">
+                <strong className="text-black font-medium text-2xl">
+                  {typeof car.price === "string" ? (
+                    <span>
+                      {new Intl.NumberFormat("pt-BR", {
+                        style: "currency",
+                        currency: "BRL",
+                        minimumFractionDigits: 0, // Definindo 0 para não exibir casas decimais
+                      }).format(parseFloat(car.price))}
+                    </span>
+                  ) : (
+                    <span>
+                      {new Intl.NumberFormat("pt-BR", {
+                        style: "currency",
+                        currency: "BRL",
+                        minimumFractionDigits: 0, // Definindo 0 para não exibir casas decimais
+                      }).format(car.price)}
+                    </span>
+                  )}
+                </strong>
+              </div>
 
-          <div className="px-2 pb-2">
-            <span className="text-zinc-700">Uberlândia - MG</span>
-          </div>
-        </section>
+              <div className="w-full h-px bg-slate-200 my-2"></div>
+
+              <div className="px-2 pb-2 text-sm">
+                <span className="text-zinc-700 flex gap-1 justify-center items-center">
+                  <GrLocation size={16} /> {car.city} - {car.state}
+                </span>
+              </div>
+            </section>
+          </Link>
+        ))}
       </main>
     </Container>
   );
