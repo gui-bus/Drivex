e0fcd4ce3e4ca5f28c5079c595e27497c0e45ded
Guilherme Bustamante <guilherme_bustamante@live.com>
2023-08-23 15:19:47 -0300

    style: change component position & remove icon

--- src/components/panelHeader/index.tsx
+++ src/components/panelHeader/index.tsx
@@ -1,16 +1,15 @@
 import { Link } from "react-router-dom";
-import { IoIosAddCircleOutline } from "react-icons/io";
 
 export function DashboardHeader() {
  
 
   return (
-    <div className="w-full flex font-medium mb-4 text-sm md:text-base items-center justify-between text-white">
+    <div className="w-full flex flex-col font-medium text-sm md:text-base items-center justify-center text-white my-5">
       <Link
-        className="flex items-center justify-center gap-2 hover:underline transform transition-transform duration-300 ease-in-out bg-mainRed px-4 py-2 rounded-lg h-10"
+        className="flex items-center justify-center gap-2 hover:bg-mainRedLighter transform transition-transform duration-300 ease-in-out bg-mainRed px-4 py-2 rounded-md h-10"
         to="/dashboard/new"
       >
-        Cadastrar veículo <IoIosAddCircleOutline size={24} />
+        Cadastrar novo veículo
       </Link>
     </div>
   );
