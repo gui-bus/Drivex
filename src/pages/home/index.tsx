19c5bfbd38d3ba791dfad93865ba7e3987b2c00b
Guilherme Bustamante <guilherme_bustamante@live.com>
2023-08-23 15:21:38 -0300

    feat: add details button & change styles

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -71,7 +71,7 @@ export function Home() {
 
   return (
     <Container>
-      <section className="bg-gray-100 py-16 px-6 rounded-lg w-full max-w-3xl mx-auto flex flex-col justify-center items-center gap-6 text-center -mt-10">
+      <section className="bg-gray-100 py-16 px-6 rounded-lg w-full max-w-3xl mx-auto flex flex-col justify-center items-center gap-6 text-center">
         <p className="text-xl text-gray-600 max-w-xl font-semibold mb-2">
           Explore a melhor seleção de carros novos e usados em todo o Brasil.
           Encontre o veículo dos seus sonhos!
@@ -91,70 +91,77 @@ export function Home() {
 
       <main className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 text-center">
         {cars.map((car) => (
-          <Link key={car.id} to={`/car/${car.id}`}>
-            <section className="w-full bg-white rounded-lg hover:scale-102 transition-all duration-200 ease-linear cursor-pointer shadow-lg">
-              <div
-                className="w-full h-56 max-h-72 rounded-lg relative"
-                style={{
-                  display: loadImages.includes(car.id) ? "none" : "block",
-                }}
-              >
-                <div className="absolute inset-0 flex items-center justify-center">
-                  <BarLoader color={"#DC3237"} />
-                </div>
+          <section
+            key={car.id}
+            className="w-full bg-white rounded-lg cursor-default shadow-lg"
+          >
+            <div
+              className="w-full h-56 max-h-72 rounded-lg relative"
+              style={{
+                display: loadImages.includes(car.id) ? "none" : "block",
+              }}
+            >
+              <div className="absolute inset-0 flex items-center justify-center">
+                <BarLoader color={"#DC3237"} />
               </div>
+            </div>
 
-              <img
-                src={car.images[0].url}
-                alt="Veiculo"
-                className="w-full rounded-t-lg mb-2 h-56 max-h-72 object-cover"
-                onLoad={() => handleImageLoad(car.id)}
-                style={{
-                  display: loadImages.includes(car.id) ? "block" : "none",
-                }}
-              />
-              <p className="font-bold my-2 px-2 uppercase lg:text-sm">
-                {car.name} {car.model}
+            <img
+              src={car.images[0].url}
+              alt="Veiculo"
+              className="w-full rounded-t-lg mb-2 h-56 max-h-72 object-cover"
+              onLoad={() => handleImageLoad(car.id)}
+              style={{
+                display: loadImages.includes(car.id) ? "block" : "none",
+              }}
+            />
+            <p className="font-bold my-2 px-2 uppercase lg:text-sm">
+              {car.name} {car.model}
+            </p>
+            <p className="text-zinc-500 px-2 text-xs uppercase">
+              {car.version}
+            </p>
+
+            <p className="text-zinc-500 px-2 text-xs">
+              {car.year} | {parseFloat(car.km).toLocaleString("pt-BR")} km
+            </p>
+
+            <div className="flex flex-col px-2 text-sm my-3">
+              <strong className="text-black font-medium text-2xl">
+                {typeof car.price === "string" ? (
+                  <span>
+                    {new Intl.NumberFormat("pt-BR", {
+                      style: "currency",
+                      currency: "BRL",
+                      minimumFractionDigits: 0, // Definindo 0 para não exibir casas decimais
+                    }).format(parseFloat(car.price))}
+                  </span>
+                ) : (
+                  <span>
+                    {new Intl.NumberFormat("pt-BR", {
+                      style: "currency",
+                      currency: "BRL",
+                      minimumFractionDigits: 0, // Definindo 0 para não exibir casas decimais
+                    }).format(car.price)}
+                  </span>
+                )}
+              </strong>
+            </div>
+
+            <div className="w-full h-px bg-slate-200 my-2"></div>
+
+            <div className="px-2 pb-2 text-sm">
+              <span className="text-zinc-700 flex gap-1 justify-center items-center">
+                <GrLocation size={16} /> {car.city} - {car.state}
+              </span>
+            </div>
+
+            <Link to={`/car/${car.id}`}>
+              <p className="py-3 bg-mainRed hover:bg-mainRedLighter transition-all duration-300 ease-in-out font-medium text-white drop-shadow-lg">
+                Ver detalhes
               </p>
-              <p className="text-zinc-500 px-2 text-xs uppercase">
-                {car.version}
-              </p>
-
-              <p className="text-zinc-500 px-2 text-xs">
-                {car.year} | {parseFloat(car.km).toLocaleString("pt-BR")} km
-              </p>
-
-              <div className="flex flex-col px-2 text-sm my-3">
-                <strong className="text-black font-medium text-2xl">
-                  {typeof car.price === "string" ? (
-                    <span>
-                      {new Intl.NumberFormat("pt-BR", {
-                        style: "currency",
-                        currency: "BRL",
-                        minimumFractionDigits: 0, // Definindo 0 para não exibir casas decimais
-                      }).format(parseFloat(car.price))}
-                    </span>
-                  ) : (
-                    <span>
-                      {new Intl.NumberFormat("pt-BR", {
-                        style: "currency",
-                        currency: "BRL",
-                        minimumFractionDigits: 0, // Definindo 0 para não exibir casas decimais
-                      }).format(car.price)}
-                    </span>
-                  )}
-                </strong>
-              </div>
-
-              <div className="w-full h-px bg-slate-200 my-2"></div>
-
-              <div className="px-2 pb-2 text-sm">
-                <span className="text-zinc-700 flex gap-1 justify-center items-center">
-                  <GrLocation size={16} /> {car.city} - {car.state}
-                </span>
-              </div>
-            </section>
-          </Link>
+            </Link>
+          </section>
         ))}
       </main>
     </Container>
