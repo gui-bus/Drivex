55d3d2ac889222f1f7568aa154e9db6a8307b568
Guilherme Bustamante <guilherme_bustamante@live.com>
2023-08-23 15:17:51 -0300

    style: change footer design

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -3,61 +3,77 @@ import { FaLinkedinIn, FaGithub, FaEnvelope } from "react-icons/fa";
 import logoImg from "../../assets/drivexLogo.png";
 
 export function Footer() {
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
   return (
-    <div className="bg-white up-shadow w-full mt-8">
+    <div className="bg-white up-shadow w-full mt-8 ">
       <footer className="max-w-7xl mx-auto py-4">
         <div className="flex flex-col items-center md:flex-row justify-between px-4">
           <div className="mb-4 md:mb-0">
             <Link to="/">
-              <img src={logoImg} alt="Logo DriveX" className="w-36 h-auto" />
+              <img src={logoImg} alt="Logo DriveX" className="w-32 h-auto" />
             </Link>
           </div>
-          <div className="text-center mb-5 md:mb-0">
-            <p className="text-darkGray ">
-              Compre e venda veículos com segurança e facilidade na DriveX.
-            </p>
-            <div className="mt-2">
-              <Link
-                to="/"
-                className="text-blue-500 hover:underline transition-colors duration-200"
-              >
-                Termos de Uso
-              </Link>{" "}
-              |{" "}
-              <Link
-                to="/"
-                className="text-blue-500 hover:underline transition-colors duration-200"
-              >
-                Política de Privacidade
-              </Link>
-            </div>
-            <p className="text-center text-darkGray mt-2">
+          <div className="text-center text-zinc-700">
+            <p className="mt-2 font-medium">
               &copy; {new Date().getFullYear()} DriveX. Todos os direitos
-              reservados.
+              reservados - Criado por Guilherme Bustamante.
             </p>
+            <div className="mt-2 space-x-4">
+              <Link
+                to="/"
+                className="text-gray-500 hover:text-mainRed transition-colors duration-200"
+              >
+                Catálogo
+              </Link>
+              <Link
+                to="/dashboard"
+                className="text-gray-500 hover:text-mainRed transition-colors duration-200"
+              >
+                Dashboard
+              </Link>
+              <Link
+                to="/dashboard/new"
+                className="text-gray-500 hover:text-mainRed transition-colors duration-200"
+              >
+                Cadastro de Veículos
+              </Link>
+              <button
+                onClick={scrollToTop}
+                className="text-gray-500 hover:text-mainRed transition-colors duration-200 focus:outline-none"
+              >
+                Voltar para o topo
+              </button>
+            </div>
           </div>
-          <div className="flex space-x-4 mb-4 md:mb-0">
+          <div className="flex space-x-4 mt-4">
             <a
               href="https://www.linkedin.com/in/gui-bus/"
               target="_blank"
               rel="noopener noreferrer"
-              className="border-2 rounded-full p-2 border-darkGray transition-colors duration-200 ease-linear hover:bg-lightGray"
             >
-              <FaLinkedinIn size={20} color="#222423" />
+              <FaLinkedinIn
+                size={24}
+                className="text-zinc-700 hover:text-mainRed transition-colors duration-200"
+              />
             </a>
             <a
               href="https://github.com/gui-bus"
               target="_blank"
               rel="noopener noreferrer"
-              className="border-2 rounded-full p-2 border-darkGray transition-colors duration-200 ease-linear hover:bg-lightGray"
             >
-              <FaGithub size={20} color="#222423" />
+              <FaGithub
+                size={24}
+                className="text-zinc-700 hover:text-mainRed transition-colors duration-200"
+              />
             </a>
-            <a
-              href="mailto:guibus.dev@gmail.com"
-              className="border-2 rounded-full p-2 border-darkGray transition-colors duration-200 ease-linear hover:bg-lightGray"
-            >
-              <FaEnvelope size={20} color="#222423" />
+            <a href="mailto:guibus.dev@gmail.com">
+              <FaEnvelope
+                size={24}
+                className="text-zinc-700 hover:text-mainRed transition-colors duration-200"
+              />
             </a>
           </div>
         </div>
